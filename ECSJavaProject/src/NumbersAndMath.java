a4dd0c0ee6c78e47c10dc78f9fa43fb3bce4ba01
Yer <ChangJ1198@sduhsd.lan>
2017-11-30 09:43:06 -0800

    created and saved

---
+++ ECSJavaProject/src/NumbersAndMath.java
@@ -0,0 +1,20 @@
+
+public class NumbersAndMath {
+	
+		
+		public static void main(String[] args)
+		{
+			//sample problem #4
+			System.out.println("I will now count my chickens:");
+			
+			System.out.println("Hens " + ( 25 + 30 / 6));
+			System.out.println("I like typing this.");
+			System.out.println("This is fun.");
+			System.out.println("Yay! Printing.");
+			System.out.println("I'd rather much you 'not'.");
+			System.out.println("I \"said\" do not touch this.");
+
+		}
+}
+
+
---
+++ ECSJavaProject/src/YourInitials.java
@@ -0,0 +1,17 @@
+
+public class YourInitials {
+	public static void main(String[] args)
+	{
+		//sample problem #3
+		System.out.println("     J    CCCCCC");
+		System.out.println("     J    C");
+		System.out.println("     J    C");
+		System.out.println("     J    C");
+		System.out.println("     J    C");
+		System.out.println("JJJJJJ    CCCCCC");
+		
+		
+		
+	
+	}
+}
