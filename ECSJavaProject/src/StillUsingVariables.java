70573a59d17123437ad47ddc5c401eb574f67d2f
Yer <ChangJ1198@sduhsd.lan>
2017-11-30 10:46:15 -0800

    finished assignment 2 variables

---
+++ ECSJavaProject/src/StillUsingVariables.java
@@ -0,0 +1,15 @@
+
+public class StillUsingVariables {
+	public static void main(String[] args)
+	{
+		//variables assignment #4
+	String myName;
+	int da;
+	myName = "Josh S. Chang";
+	da = 2020;
+	System.out.println("my name is " + myName + " and I will graduate in " + da + ".");
+	
+	
+	
+	}
+}
