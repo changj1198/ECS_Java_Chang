a77a8b990a5e5a9860f88ce0270b2b2afd722d80
Yer <ChangJ1198@sduhsd.lan>
2017-12-01 10:17:04 -0800

    changed to public static void main

--- ECSJavaProject/src/main.java
+++ ECSJavaProject/src/main.java
@@ -2,13 +2,13 @@
 public class main {
 
 	public static void main(String[] args) {
-		VariablesAndNames helper = new VariablesAndNames();
-		uV h = new uV();
-		MoreVN g = new MoreVN(); 
-		helper.meow();
-		h.ok();
-		g.dare();
-		System.out.println("a ok");
+		// VariablesAndNames helper = new VariablesAndNames();
+		//uV h = new uV();
+		//MoreVN g = new MoreVN(); 
+		//helper.meow();
+		//h.ok();
+		//g.dare();
+		//System.out.println("a ok");
 		
 	}
 	
